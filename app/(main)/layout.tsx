effa8a25ccc5e115ca88b5335e98e958018d61bc
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2025-10-27 11:08:36 +0530

    Add Meta Pixel tracking code to layout. Integrate Facebook Pixel for enhanced analytics and user tracking capabilities.

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import "@/app/globals.css";
 import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { GoogleAnalytics } from "@next/third-parties/google";
@@ -38,6 +39,36 @@ export default function RootLayout({
         <GoogleAnalytics gaId="G-YF108ZPTLG" />
       </head>
       <body className="font-inter">
+        {/* Meta Pixel Code */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '367591980504934');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=367591980504934&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
         <Header />
         {children}
         <SupportHelpSection />
